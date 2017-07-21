21b5c6481026239c8e9de07d756524bf421df85c
Lagreze <lagrezd@gmail.com>
2017-07-21 18:30:52 +0200

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app light>
+      <v-toolbar dark class="primary">
+          <v-toolbar-side-icon></v-toolbar-side-icon>
+          <v-toolbar-title light>Vue Analytics</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-text-field
+                  label="Insérer votre Oauth Google client ID..."
+                  single-line
+                  append-icon="search"
+                  dark
+                  hide-details
+          ></v-text-field>
+      </v-toolbar>
+      <v-container>
+          <router-view></router-view>
+      </v-container>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'bubble_chart', title: 'Inspire' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Vuetify.js'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.text-center {
+    margin:0 auto;
+}
+</style>
